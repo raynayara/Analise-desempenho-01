3bbe20daee0f41d005affb15758c150b584240c6
raynayara <raysllannayara10@gmail.com>
2024-11-10 19:58:03 -0300

    correção

--- src/Main.java
+++ src/Main.java
@@ -10,23 +10,32 @@ public class Main {
 
         try (BufferedReader br = new BufferedReader(new FileReader("arq.txt"))) {
             
+            // Leitura dos valores iniciais e adição na lista
             for (String valor : br.readLine().split(" ")) {
                 lista.adicionar(Integer.parseInt(valor));
             }
             
+            // Leitura e processamento das ações
             int numAcoes = Integer.parseInt(br.readLine().trim());
             for (int i = 0; i < numAcoes; i++) {
                 String[] partes = br.readLine().split(" ");
-                switch (partes[0]) {
+                String acao = partes[0];
+
+                switch (acao) {
                     case "A":
                         lista.adicionar(Integer.parseInt(partes[1]));
+                        System.out.println("Ação: Adicionar | Valor adicionado: " + partes[1]);
                         break;
                     case "R":
                         lista.remover(Integer.parseInt(partes[1]));
+                        System.out.println("Ação: Remover | Valor removido: " + partes[1]);
                         break;
                     case "P":
+                        System.out.println("Ação: Imprimir");
                         lista.imprimir();
                         break;
+                    default:
+                        System.out.println("Ação desconhecida: " + acao);
                 }
             }
 
@@ -35,4 +44,3 @@ public class Main {
         }
     }
 }
-
